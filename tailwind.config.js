07045b8e2b9e7c4e7b1d72ee5ba40d1c410e9602
Luke Nielsen <luke.nielsenn@gmail.com>
2024-11-14 15:05:40 -0700

    chaged "public" directory to "docs"

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     content: [
       "./src/**/*.{html,js,jsx,ts,tsx}", // No need to include image or CSS files
-      "./public/*.html",
+      "./docs/*.html",
     ],
     theme: {
       extend: {
